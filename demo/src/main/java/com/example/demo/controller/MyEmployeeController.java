919cee0bfa7ccb0fd0b91b70aa10bddf6284bb86
RajeshReddy21 <rajeshchalla573@gmail.com>
2025-02-09 01:15:24 +0530

    Update MyEmployeeController.java

--- demo/src/main/java/com/example/demo/controller/MyEmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/MyEmployeeController.java
@@ -1,24 +1,19 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.pojo.Employee;
 import com.example.demo.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class MyEmployeeController {
 	
-	 
+	 private Logger logger= LoggerFactory.getLogger(MyEmployeeController.class);
 	  @Autowired
 	  private EmployeeService service ;
 	
@@ -27,8 +22,10 @@ public class MyEmployeeController {
 		
 		  if(employee !=null) {
 			  service.saveEmp(employee);
+			  logger.info("Employee details Successfylly Saved "+ employee);
 			  return "Congratulation Your Successfylly Saved...!";
 		  }else {
+			  logger.info("Employee details not save "+ employee);
 			 return "Sorry You are not saved....!";
 		  }
 	}
